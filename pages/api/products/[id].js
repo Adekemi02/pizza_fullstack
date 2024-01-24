78239f85ab7da5678008461514bc49935fd39298
Adisa Barakat <adisabarakatadekemi@gmail.com>
2024-01-24 01:36:03 +0100

    Modifies backend

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,5 +1,5 @@
-import dbConnect from "@/utils/dbConnect";
-import Product from "@/models/Product";
+import dbConnect from "@/pages/api/utils/dbConnect";
+import Product from "@/pages/api/models/Product";
 
 
 export default async function handler(req, res) {
