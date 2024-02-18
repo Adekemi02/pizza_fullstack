ec148c15d77f42dc26b7b9d1297859b001d1f6dc
Adisa Barakat <adisabarakatadekemi@gmail.com>
2024-02-18 16:07:30 +0100

    modifies login

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import axios from 'axios';
 
 import styles from '@/styles/Login.module.css'
@@ -6,24 +6,11 @@ import styles from '@/styles/Login.module.css'
 
 
 const login = () => {
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [error, setError] = useState(false);
+  
   
 
 
-  const handleClick = async () => {
-    try {
-      await axios.post("http://localhost:3000/api/login", {
-        username,
-        password,
-      });
-      router.push("/admin");
-
-    } catch (err) {
-      setError(true);
-    }
-  };
+  
 
   return (
 
@@ -35,17 +22,17 @@ const login = () => {
           <input
             placeholder="username"
             className={styles.input}
-            onChange={(e) => setUsername(e.target.value)}
+            // onChange={(e) => setUsername(e.target.value)}
           />
 
           <input
             placeholder="password"
             type="password"
             className={styles.input}
-            onChange={(e) => setPassword(e.target.value)}
+            // onChange={(e) => setPassword(e.target.value)}
           />
 
-          <button onClick={handleClick} className={styles.button}>
+          <button  className={styles.button}>
             Sign In
           </button>
         </div>
