5bf21e5408e4bbd6b2360e330fe530d7f9a9aa56
Adisa Barakat <adisabarakatadekemi@gmail.com>
2024-02-18 16:04:53 +0100

    Modifies login page

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,65 +1,65 @@
-// import React, {useState} from 'react';
-// import axios from 'axios';
-// import { useRouter } from 'next/router';
-// import styles from '@/styles/Login.module.css'
+import React, {useState} from 'react';
+import axios from 'axios';
+
+import styles from '@/styles/Login.module.css'
 
 
 
-// const login = () => {
-//   const [username, setUsername] = useState(null);
-//   const [password, setPassword] = useState(null);
-//   const [error, setError] = useState(false);
-//   const router = useRouter();
-
-
-//   const handleClick = async () => {
-//     try {
-//       await axios.post("http://localhost:3000/api/login", {
-//         username,
-//         password,
-//       });
-//       router.push("/admin");
-
-//     } catch (err) {
-//       setError(true);
-//     }
-//   };
-
-//   return (
-
-//     <div className={styles.container}>
-//       <div >
-//         <h1 className="text-center font-semibold text-xl mb-3">Admin Dashboard</h1>
-        
-//         <div className={styles.wrapper}>
-//           <input
-//             placeholder="username"
-//             className={styles.input}
-//             onChange={(e) => setUsername(e.target.value)}
-//           />
-
-//           <input
-//             placeholder="password"
-//             type="password"
-//             className={styles.input}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-
-//           <button onClick={handleClick} className={styles.button}>
-//             Sign In
-//           </button>
-//         </div>
-        
-
-        
-
-        
-
-//         {error && <span className={styles.error}>Wrong Credentials!</span>}
-//       </div>
-//     </div>
-//   );
+const login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(false);
   
-// }
 
-// export default login;
+
+  const handleClick = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/login", {
+        username,
+        password,
+      });
+      router.push("/admin");
+
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+
+    <div className={styles.container}>
+      <div >
+        <h1 className="text-center font-semibold text-xl mb-3">Admin Dashboard</h1>
+        
+        <div className={styles.wrapper}>
+          <input
+            placeholder="username"
+            className={styles.input}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            placeholder="password"
+            type="password"
+            className={styles.input}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button onClick={handleClick} className={styles.button}>
+            Sign In
+          </button>
+        </div>
+        
+
+        
+
+        
+
+        {error && <span className={styles.error}>Wrong Credentials!</span>}
+      </div>
+    </div>
+  );
+  
+}
+
+export default login;
